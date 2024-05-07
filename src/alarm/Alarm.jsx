12eede958e33fc17e18185c4db403a96e6fd6874
dani <danifromecuador@gmail.com>
2024-05-07 12:30:11 -0500

    Play a sound every interval time if the alarm is ON | alarm | root/ |

--- src/alarm/Alarm.jsx
+++ src/alarm/Alarm.jsx
@@ -1,13 +1,14 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { store } from '../store/store.js'
+import alarmSound from '../../public/alarm.wav'
 import './Alarm.css'
 
 export const Alarm = () => {
   const { alarmStore, timeStore } = store()
+  const audio = new Audio(alarmSound)
   const prevMinuteRef = useRef(new Date().getMinutes())
-
-  // update time each minute, avoid updating more than necessary
-  console.log("  Alarm was rendered")
+  const [hide1, setHide1] = useState("")
+  const [hide2, setHide2] = useState("hide")
 
   useEffect(() => {
     alarmStore.updateNextInterval()
@@ -15,6 +16,15 @@ export const Alarm = () => {
       const currentMinute = new Date().getMinutes()
       if (prevMinuteRef.current !== currentMinute) {
         timeStore.updateTime()
+        if (currentMinute % alarmStore.interval === 0 && alarmStore.alarmState === "ON") {
+          audio.play()
+          setHide1("hide")
+          setHide2("")
+          setTimeout(() => {
+            setHide1("")
+            setHide2("hide")
+          }, 8000);
+        }
         prevMinuteRef.current = currentMinute
       }
     }, 1000)
@@ -35,8 +45,8 @@ export const Alarm = () => {
         <p>{timeStore.time.dateStr.h}:{timeStore.time.dateStr.m}</p>
       </div>
       <div>
-        <p>Next alarm will sound at {alarmStore.nextInterval.h}:{alarmStore.nextInterval.m}</p>
-        <p className='hide'>Alarm is playing right now</p>
+        <p className={hide1}>Next alarm will sound at {alarmStore.nextInterval.h}:{alarmStore.nextInterval.m}</p>
+        <p className={hide2}>Alarm is playing right now</p>
         <p className='hide'>Can&apos;t you hear the alarm? Click here</p>
       </div>
     </div>
