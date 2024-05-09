4f7f9dcbdf01bcc5bd5c851c362d1f61d61f4236
dani <danifromecuador@gmail.com>
2024-05-09 11:02:59 -0500

    Create a Todo component | todo | main/App.jsx & src/ |

--- main/App.jsx
+++ main/App.jsx
@@ -1,11 +1,13 @@
 
 import { Alarm } from '../src/alarm/Alarm'
+import { Todo } from '../src/todo/Todo'
 import './App.css'
 
 export const App = () => {
   return (
     <div className="App">
       <Alarm />
+      <Todo />
     </div>
   )
 }
