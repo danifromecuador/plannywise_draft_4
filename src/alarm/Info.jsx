6de5aefa8c51416b95a00c017bd1c9a387edbc27
dani <danifromecuador@gmail.com>
2024-05-09 10:52:16 -0500

    Update issue link for Alarm sound error | alarm | src/alarm/Info.jsx |

--- src/alarm/Info.jsx
+++ src/alarm/Info.jsx
@@ -15,7 +15,7 @@ export const Info = () => {
         </div>
         <ul>
           <li>If you enjoy this app, please <a href="https://github.com/danifromecuador/plannywise" target='blank'>give it a ⭐</a></li>
-          <li>If you&apos;re not hearing the alarm sound, please check out <a href="https://github.com/danifromecuador/plannywise/issues/8" target='blank'>this issue</a></li>
+          <li>If you&apos;re not hearing the alarm sound, please check out <a href="https://github.com/danifromecuador/plannywise_draft/issues/8" target='blank'>this issue</a></li>
           <li>If you&apos;d like to provide feedback, report issues, or suggest improvements, <a href="https://github.com/danifromecuador/plannywise/issues" target='blank'>click here</a></li>
         </ul>
       </div>
