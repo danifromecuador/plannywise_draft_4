33eff123ad30988c7d3604ce66532c239a3d2b89
dani <danifromecuador@gmail.com>
2024-05-17 14:23:37 -0500

    Add a previous interval key to the Completed-Tasks-List | tracker | src/completedTasks/ |

--- src/completedTasks/CompletedTasks.jsx
+++ src/completedTasks/CompletedTasks.jsx
@@ -1,21 +1,23 @@
 import { useState, useEffect } from 'react'
 import PropTypes from "prop-types";
 import { Store } from '../store/store'
+import { previousInterval } from '../store/alarm';
 import './CompletedTasks.css'
 
 export const CompletedTasks = ({ storeLocal }) => {
   const [input, setInput] = useState("")
   const [hide1, setHide1] = useState("")
+  const previousIntervalStr = `${previousInterval().hi}:${previousInterval().mi}  -  ${previousInterval().hf}:${previousInterval().mf}`
   let doneSize = storeLocal.dones.length
   let workedHours = doneSize / 4
-  const handleEnter = e => e.key === "Enter" && (storeLocal.addTodo(input), setInput(""))
+  const handleEnter = e => e.key === "Enter" && (storeLocal.addTodo(previousIntervalStr, input), setInput(""))
 
   useEffect(() => {
     localStorage.setItem("completedTasks", JSON.stringify(Store.getState().tasks.dones))
   }, [storeLocal])
 
   return (
-    <div className='Todo main-component'>
+    <div className='CompletedTasks Todo main-component'>
       <div className="header">
         <div className={`header-left ${doneSize === 0 ? "hide" : ""}`}>Worked Hours: <span className='completed'>{workedHours}</span></div>
         <div className='header-center' onClick={() => { hide1 === "" ? setHide1("hide") : setHide1("") }}>Completed Tasks</div>
@@ -23,19 +25,23 @@ export const CompletedTasks = ({ storeLocal }) => {
       </div>
       <ul className={`todos-dones secondary-component ${doneSize === 0 ? "hide" : ""} ${hide1}`}>
         {storeLocal.dones.map(e => (
-          <li key={e.index} className='dones'>{e.content}</li>
+          <li key={e.index} className='dones'>{e.interval} {"=>"} {e.content}</li>
         ))}
       </ul>
       <div className={`btn-input ${hide1}`}>
         <button className='button' onClick={() => storeLocal.removeAllCompleted()}>Delete All Tasks</button>
-        <input
-          type="text"
-          placeholder='Type a completed task and press Enter'
-          className='button'
-          value={input}
-          onChange={e => setInput(e.target.value)}
-          onKeyDown={e => handleEnter(e)}
-        />
+
+        <div className='interval-input'>
+          <span>{previousIntervalStr}</span>
+          <input
+            type="text"
+            placeholder='Type a completed task and press Enter'
+            className='button'
+            value={input}
+            onChange={e => setInput(e.target.value)}
+            onKeyDown={e => handleEnter(e)}
+          />
+        </div>
       </div>
     </div>
   )
